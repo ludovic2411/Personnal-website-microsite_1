3c18febfd5f73ed85139b9deea7836a1da8db688
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-04-30 18:39:25 +0100

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Choose your language</title>
+  </head>
+  <body>
+    <h1>please choose your language</h1>
+    <section class="wrapper">
+      <form class="index_form" action="index.html" method="post">
+        <input type="text" name="french_submit" value="French">
+        <input type="text" name="english_submit" value="English">
+      </form>
+    </section>
+  </body>
+</html>
